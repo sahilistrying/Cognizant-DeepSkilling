41be02e53e91dbf02ca9504b0da285c28089a9b3
sahilistrying <sahil24072005@gmail.com>
2026-07-07 10:15:00 +0530

    feat: integrated Week 3 Spring REST and data modules

---
+++ Week 3/Spring REST Using Spring Boot/1.Spring REST hands-on/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -0,0 +1,40 @@
+﻿package com.cognizant.spring_learn;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+@SpringBootApplication
+public class SpringLearnApplication {
+	private static final Logger LOGGER = LoggerFactory.getLogger(SpringLearnApplication.class);
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLearnApplication.class, args);
+		System.out.print("\n\nSpring Application Started\n");
+		displayDate();
+		displayCountry();
+	}
+	public static void displayDate() {
+		LOGGER.info("START");
+		ApplicationContext context = new ClassPathXmlApplicationContext("date-format.xml");
+		SimpleDateFormat format = context.getBean("dateFormat", SimpleDateFormat.class);
+		try {
+			Date date = format.parse("31/12/2018");
+			LOGGER.debug("\n\n Parsed Date: {}\n", date);
+		} catch (Exception e) {
+			LOGGER.error(e.getMessage());
+		}
+		LOGGER.info("END");
+	}
+	public static void displayCountry() {
+		LOGGER.info("START Country");
+		ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+		Country country = context.getBean("country", Country.class);
+		LOGGER.debug("Country : {}", country.toString());
+		LOGGER.info("END");
+	}
+}
+
